b0603f6b95fa32d9722528cc68f18624449d136e
Beyza <erdembeyza.be@gmail.com>
2026-03-12 12:13:55 +0300

    matris okuma dosyası eklendi

---
+++ onikimart/matrisokuma.py
@@ -0,0 +1,9 @@
+import cv2 as cv
+from matplotlib import pyplot as plt
+path = r"C:\Users\beyza\Desktop\goruntu_isleme_dersi\image\opencv.png"
+
+img_color=cv.imread(path, cv.IMREAD_COLOR)
+img_gray= cv.imread(path, cv.IMREAD_GRAYSCALE )
+
+print(f"renkli boyut: {img_color.shape}")
+print(f"gri boyut:{img_gray.shape}")
