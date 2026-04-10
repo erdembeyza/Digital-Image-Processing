9550c5796899dcb763962f6be0aa6424d06574e6
Beyza <erdembeyza.be@gmail.com>
2026-04-10 22:40:42 +0300

    roi güncellendi+ resim kaydedildi

--- onikimart/roi_cikarma.py
+++ onikimart/roi_cikarma.py
@@ -1,11 +1,23 @@
 import cv2 as cv
 from matplotlib import pyplot as plt
+import numpy as np
 path = r"C:\Users\beyza\Desktop\goruntu_isleme_dersi\image\opencv.png"
+
 img = cv.imread(path)
 img_color=cv.imread(path, cv.IMREAD_COLOR)
 img_gray= cv.imread(path, cv.IMREAD_GRAYSCALE )
 
-roi = img[50:200, 100:300]
-cv.imshow("ilgi alanı çıkarma", roi)
-img[0:150,0:200]=roi
+# Define roi as an empty numpy array
+roi = np.zeros((150, 200, 3), dtype=np.uint8)
+
+# Resize roi to the desired dimensions
+roi = cv.resize(roi, (200, 150))
+
+# Assign roi to the specific region of img
+img[0:150, 0:200] = roi
+
+cv.imshow("Yapistirilmis", img)
+dosya_adi = f"roi_cikarma.png"
+cv.imwrite(dosya_adi, img)
 cv.waitKey()
+cv.destroyAllWindows()
