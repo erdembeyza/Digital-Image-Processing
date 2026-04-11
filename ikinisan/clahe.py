af791511d51ab9a9bc5bc36409d2767ce656536f
Beyza <erdembeyza.be@gmail.com>
2026-04-11 19:31:18 +0300

    standart histogram ve clache eklendi

--- ikinisan/clahe.py
+++ ikinisan/clahe.py
@@ -1,15 +1,16 @@
 import os
 """standart histogram: görüntünün tamamına (global) bakar ve karanlık fotoğrafları aydınlatmak veya 
 soluk fotoğrafların kontrastını artırmak için pikselleri 0-255 arasına yayar.
-clache: sh nin yapamadığı kısımları yapar: lokal kontrast artırma, gürültü koruma, aşırı aydınlatılmış 
-bölgeleri tıraşlama gibi işlemleri yapar."""
+clache: sh nin yapamadığı kısımları yapar: lokal kontrast artırma, gürültü koruma, aşırı aydınlatılmış bölgeleri tıraşlama gibi işlemleri yapar."""
 
 import cv2
 import numpy as np
 path= r"C:\Users\beyza\Desktop\goruntu_isleme_dersi\ikinisan\sisligece.jpg"
 img= cv2.imread(path, 0)
-"""burdaki 0ı unutma!"""
+"""burdaki 0ı unutma!, çünkü histogram eşitleme ve clahe gibi yoğunluk dönüşümleri gri tonlamalı fotoğraflar 
+üzerinde çalışır."""
 
+img_orginal = cv2.imread(path)
 hist_data = cv2.calcHist([img], [0], None, [256], [0, 256])
 # Kolayı: plt.hist(img.ravel(), 256, [0, 256]) 
 
@@ -23,7 +24,10 @@ clahe_img = clahe_obj.apply(img)
 
 cv2.imwrite(os.path.join(os.path.dirname(path), "global_eq.jpg"), global_eq)
 cv2.imwrite(os.path.join(os.path.dirname(path), "clahe.jpg"), clahe_img)
+cv2.imwrite(os.path.join(os.path.dirname(path), "orginal.jpg"), img)
 
 cv2.imshow('orginal', img)
+cv2.imshow("global_eq", global_eq)
+cv2.imshow("img_orginal", img_orginal)
 cv2.imshow('CLAHE (Mucize!)', clahe_img)
 cv2.waitKey(0)
