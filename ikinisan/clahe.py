87a1b79165e95ca67cb4fc9fbd6b67edc7fbfc46
Beyza <erdembeyza.be@gmail.com>
2026-04-11 19:12:20 +0300

    standart histogram ve clache eklendi

--- ikinisan/clahe.py
+++ ikinisan/clahe.py
@@ -1,3 +1,8 @@
+import os
+"""standart histogram: görüntünün tamamına (global) bakar ve karanlık fotoğrafları aydınlatmak veya 
+soluk fotoğrafların kontrastını artırmak için pikselleri 0-255 arasına yayar.
+clache: sh nin yapamadığı kısımları yapar: lokal kontrast artırma, gürültü koruma, aşırı aydınlatılmış bölgeleri tıraşlama gibi işlemleri yapar."""
+
 import cv2
 import numpy as np
 path= r"C:\Users\beyza\Desktop\goruntu_isleme_dersi\ikinisan\sisligece.jpg"
@@ -14,6 +19,9 @@ global_eq = cv2.equalizeHist(img)
 # clipLimit: Tıraşlanacak max frekans. tileGridSize: lokal fayans matris
 clahe_obj = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
 clahe_img = clahe_obj.apply(img)
+
+cv2.imwrite(os.path.join(os.path.dirname(path), "global_eq.jpg"), global_eq)
+cv2.imwrite(os.path.join(os.path.dirname(path), "clahe.jpg"), clahe_img)
 
 cv2.imshow('orginal', img)
 cv2.imshow('CLAHE (Mucize!)', clahe_img)
