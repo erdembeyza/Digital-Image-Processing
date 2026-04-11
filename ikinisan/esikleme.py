4d1b990a2392b55662bf416daad0eb5a48244895
Beyza <erdembeyza.be@gmail.com>
2026-04-11 19:07:40 +0300

    esikleme eklendi

--- ikinisan/esikleme.py
+++ ikinisan/esikleme.py
@@ -1,18 +1,24 @@
+"""fotoğrafı basitleştirerek "önemli olanı" (nesneyi) "önemsiz olandan" (arka plandan) ayırmaya yarayan 
+en temel bölütleme (segmentasyon) tekniğidir.
+Temel amacı karmaşık, çok renkli veya gri tonlamalı bir görüntüyü sadece siyah (0) ve beyaz (255) 
+olmak üzere iki renge (ikili/binary formata) indirgemektir."""
+import os
 import cv2
 import numpy as np
 path= r"C:\Users\beyza\Desktop\goruntu_isleme_dersi\ikinisan\belge.jpg"
-img= cv2.imread(path)
-
+img= cv2.imread(path, 0)
 binary = cv2.threshold(
     img, 127, 255, 
     cv2.THRESH_BINARY
 )
-
 # Otomatik Akıllı Eşik (Otsu)
 otsu = cv2.threshold(
     img, 0, 255, 
     cv2.THRESH_BINARY + cv2.THRESH_OTSU
 )
+
+cv2.imwrite(os.path.join(os.path.dirname(path), "binary.jpg"), binary[1])
+cv2.imwrite(os.path.join(os.path.dirname(path), "otsu.jpg"), otsu[1])
 
 cv2.imshow("orginal", img)
 cv2.imshow("binary", binary[1])
---
+++ ikinisan/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+import cv2
+import numpy as np
+path= r"C:\Users\beyza\Desktop\goruntu_isleme_dersi\ikinisan\belge.jpg"
+img= cv2.imread(path)
